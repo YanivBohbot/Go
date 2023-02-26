198eeba339d0b617078f1d1f7b2dce272da2d5a5
YanivBohbot <Yanivbohbot5@gmail.com>
2023-02-26 15:38:09 +0200

    adding new features get time from unix nano

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
-<<<<<<< HEAD
 func main() {
 
 	//cards := newCard()
 
 	//hand, remainingCards := deal(cards,5)
 
+	// cards := newDeck()
+	// // fmt.Println(cards.toString())
+	// // greeting := "oijweoijewf"
+	// // fmt.Println([]byte(greeting))
+	// cards.saveToFile("my_cards")
+	// cards := newDeckFromFile("my_card")
+	// cards.print()
 	cards := newDeck()
-	// fmt.Println(cards.toString())
-	// greeting := "oijweoijewf"
-	// fmt.Println([]byte(greeting))
-	cards.saveToFile("my_cards")
-=======
-import "fmt"
+	cards.shuffle()
+	cards.print()
 
-func main() {
-	fmt.Println("Hello wolrd")
->>>>>>> 16f5060bf3991163e34a8b34f361a18a6dfa6dfe
 }
